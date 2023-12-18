e112e41c94f500e7ddc932f4062c7f5e9a68e7d8
Dhruv <59387969+druvdub@users.noreply.github.com>
2023-12-19 05:28:25 +0530

    Update tsconfig for module aliases + Restructure
    Move files and update directories for components and lib and add logo

--- uomdss/app/layout.tsx
+++ uomdss/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "UoMDSS",
+  title: "University of Manchester's Data Science Society",
   description: "University of Manchester's Data Science Society",
 };
 
