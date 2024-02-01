e5254c43718de9658b6427202474205fd2d71921
Dhruv <59387969+druvdub@users.noreply.github.com>
2024-02-01 20:45:34 +0000

    Update events carousel

--- uomdss/lib/events.ts
+++ uomdss/lib/events.ts
@@ -2,6 +2,7 @@ interface Event {
   title: string;
   description: string;
   date: string;
+  location?: string;
   type: string;
 }
 
@@ -11,18 +12,21 @@ const UpcomingEvents: Event[] = [
   //   description:
   //     "This is a description of event 1. Longer description to test the layout.",
   //   date: "1st February 2024",
+  //   location: "University of Manchester",
   //   type: "workshop",
   // },
   // {
   //   title: "Event 2",
   //   description: "This is a description of event 2",
   //   date: "7th February 2024",
+  //   location: "University of Manchester",
   //   type: "social",
   // },
   // {
   //   title: "Event 3",
   //   description: "This is a description of event 3",
   //   date: "14th February 2024",
+  //   location: "University of Manchester",
   //   type: "workshop",
   // },
 ];
