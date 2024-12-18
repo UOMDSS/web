73185125a2bc1bc60f906365705ab9cc3fa64d5a
Taha Ejaz <tahaejaz22@icloud.com>
2024-12-18 16:25:41 +0000

    Added events for the last few weeks of sem 1

--- lib/events.ts
+++ lib/events.ts
@@ -33,8 +33,38 @@ const UpcomingEvents: Event[] = [
       advancements in AI and machine perception.`,
     registrationLink: 'https://forms.gle/T4PbjvJBPM8i1trF6',
     date: "6th November 2024",
-    location: "Kilburn Building Theatre 1.1",
+    location: "Kilburn Building, Theatre 1.1",
     type: "spotlight",
+  },
+  {
+    title: "Intro to Deep Learning",
+    description: 
+      `Join the Data Science Society (MUDS) for an engaging workshop on 
+      deep learning and its real-world applications in Sign Language 
+      Recognition. Whether you're new to AI or have some experience, this 
+      hands-on session will guide you through building neural networks using 
+      PyTorch. Learn how Artificial Neural Networks (ANNs) can drive impactful 
+      innovations in accessible communication. Plus, don't miss our book 
+      giveaway: "Why Machines Learn" by Anil Ananthaswamy!`,
+    registrationLink: "https://docs.google.com/forms/d/e/1FAIpQLSfXGRjjrjlzjr_KXVpQsUDDQGsk3-qrbaMZaoxOAAzoxjE7YQ/viewform",
+    date: "20th November 2024",
+    location: "Kilburn Building, Theater 1.1",
+    type: "workshop",
+  },
+  {
+    title: "Convolutional Neural Networks (CNNs)",
+    description: 
+      `The third MUDS workshop explores the transformative power of 
+      Convolutional Neural Networks (CNNs) in Medical Image Analysis. This 
+      intermediate-level session will cover the fundamentals of CNNs, building 
+      a Computer Vision pipeline with PyTorch, and training a CNN to classify 
+      organs from CT scans. Enhance your skills in computer vision tools that 
+      are revolutionizing radiology. Perfect for those passionate about 
+      healthcare, AI, or both!`,
+    registrationLink: "https://lnkd.in/eGAfpna9",
+    date: "27th November 2024",
+    location: "Simon Building, Theatre D",
+    type: "workshop",
   }
   // {
   //   title: "Event 3",
